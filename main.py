44b1ad29f044207f5d7177091a94579fbc7a1fff
multipitch <14188124+multipitch@users.noreply.github.com>
2025-02-08 11:37:00 +0000

    Reduce dimensionality of problem.

--- main.py
+++ main.py
@@ -20,22 +20,32 @@ from pydantic import BaseModel
 MAX_PLAYERS_FROM_ANY_COUNTRY = 4
 SUPERSUB_MULTIPLIER = 3
 CAPTAIN_MULTIPLIER = 2
-JERSEY_POSITIONS = {
-    1: "Prop",
-    2: "Hooker",
-    3: "Prop",
-    4: "Second-Row",
-    5: "Second-Row",
-    6: "Back-Row",
-    7: "Back-Row",
-    8: "Back-Row",
-    9: "Scrum-Half",
-    10: "Fly-Half",
-    11: "Back Three",
-    12: "Centre",
-    13: "Centre",
-    14: "Back Three",
-    15: "Back Three",
+# JERSEY_POSITIONS = {
+#     1: "Prop",
+#     2: "Hooker",
+#     3: "Prop",
+#     4: "Second-Row",
+#     5: "Second-Row",
+#     6: "Back-Row",
+#     7: "Back-Row",
+#     8: "Back-Row",
+#     9: "Scrum-Half",
+#     10: "Fly-Half",
+#     11: "Back Three",
+#     12: "Centre",
+#     13: "Centre",
+#     14: "Back Three",
+#     15: "Back Three",
+# }
+POSITIONS = {
+    "Prop": [1, 3],
+    "Hooker": [2],
+    "Second-Row": [4, 5],
+    "Back-Row": [6, 7, 8],
+    "Scrum-Half": [9],
+    "Fly-Half": [10],
+    "Centre": [12, 13],
+    "Back Three": [11, 14, 15],
 }
 
 
@@ -75,11 +85,14 @@ class Model:
     def __init__(self, dataset: Dataset) -> None:
         self.dataset = dataset
 
+        self.players = self.dataset.starting_players
         self.players_by_country = defaultdict(list)
+        self.players_by_position = defaultdict(list)
         self.predicted_player_points = {}
-        for player_name, player in self.dataset.starting_players.items():
-            self.players_by_country[player.country].append(player_name)
-            self.predicted_player_points[player_name] = (
+        for name, player in self.players.items():
+            self.players_by_country[player.country].append(name)
+            self.players_by_position[player.position].append(name)
+            self.predicted_player_points[name] = (
                 player.points + (player.adjust or 0)
             ) * self.dataset.country_weights[player.country]
 
@@ -101,9 +114,8 @@ class Model:
             ) * self.dataset.country_weights[self.supersub_country]
 
         self.problem: LpProblem
-        self.players_in_jerseys: dict[tuple[str, int], LpVariable]
+        self.players_are_selected: dict[str, LpVariable]
         self.players_are_captain: dict[str, LpVariable]
-
         self.team: dict[int, str]
         self.captain: str
         self.score: float
@@ -120,97 +132,81 @@ class Model:
         self.define_decision_variables()
         self.define_objective()
         self.constrain_budget()
-        self.constrain_jerseys_per_player()
-        self.constrain_jerseys_per_country()
+        self.constrain_players_per_country()
+        self.constrain_players_per_position()
         self.constrain_number_of_captains()
-        self.constrain_players_per_jersey()
 
     def define_decision_variables(self) -> None:
         """Define decision variables."""
-        self.players_in_jerseys = {
-            (player_name, jersey): (
-                LpVariable(name=f"{player_name} wears number {jersey}", cat=LpBinary)
-                if player.position == position
-                else 0
-            )
-            for player_name, player in self.dataset.starting_players.items()
-            for jersey, position in JERSEY_POSITIONS.items()
+        self.players_are_selected = {
+            name: LpVariable(name=f"{name} is selected", cat=LpBinary)
+            for name in self.players
         }
         self.players_are_captain = {
-            player_name: LpVariable(name=f"{player_name} is captain", cat=LpBinary)
-            for player_name in self.dataset.starting_players
+            name: LpVariable(name=f"{name} is captain", cat=LpBinary)
+            for name in self.players
         }
 
     def define_objective(self) -> None:
         """Define objective function; total points for selected team."""
         self.problem += (
-            self.predicted_supersub_points * SUPERSUB_MULTIPLIER
-            + sum(
-                self.predicted_player_points[player_name]
-                * self.players_in_jerseys[player_name, jersey]
-                for player_name in self.dataset.starting_players
-                for jersey in JERSEY_POSITIONS
+            sum(
+                self.predicted_player_points[name] * self.players_are_selected[name]
+                for name in self.players
             )
             + sum(
-                self.predicted_player_points[player_name]
-                * self.players_are_captain[player_name]
-                for player_name in self.dataset.starting_players
+                self.predicted_player_points[name] * self.players_are_captain[name]
+                for name in self.players
             )
             * (CAPTAIN_MULTIPLIER - 1)
+            + self.predicted_supersub_points * SUPERSUB_MULTIPLIER
         )
 
     def constrain_budget(self) -> None:
         """Ensure selection is within budget limit."""
         self.problem += self.dataset.budget - self.supersub_cost >= sum(
-            self.dataset.starting_players[player_name].cost
-            * self.players_in_jerseys[player_name, jersey]
-            for player_name in self.dataset.starting_players
-            for jersey in JERSEY_POSITIONS
+            self.players[name].cost * self.players_are_selected[name]
+            for name in self.players
         )
 
-    def constrain_jerseys_per_player(self) -> None:
-        """Ensure every player wears at most one jersey."""
-        for player_name in self.dataset.starting_players:
-            self.problem += 1 >= sum(
-                self.players_in_jerseys[player_name, jersey]
-                for jersey in JERSEY_POSITIONS
-            )
-
-    def constrain_jerseys_per_country(self) -> None:
-        """Apply a limit on jerseys per team (include supersub)."""
+    def constrain_players_per_country(self) -> None:
+        """Apply a limit on players per country (include supersub)."""
         for country in self.dataset.country_weights:
             self.problem += sum(
-                self.players_in_jerseys[player_name, jersey]
-                for jersey in JERSEY_POSITIONS
-                for player_name in self.players_by_country[country]
+                self.players_are_selected[name]
+                for name in self.players_by_country[country]
             ) <= MAX_PLAYERS_FROM_ANY_COUNTRY - (self.supersub_country == country)
+
+    def constrain_players_per_position(self) -> None:
+        """Can't have too many players in any position."""
+        for position, numbers in POSITIONS.items():
+            self.problem += len(numbers) >= sum(
+                self.players_are_selected[name]
+                for name in self.players_by_position[position]
+            )
 
     def constrain_number_of_captains(self) -> None:
         """Ensure there is exactly one captain."""
         # Zero captains is allowed, but can never be optimal.
         self.problem += sum(self.players_are_captain.values()) == 1
 
-    def constrain_players_per_jersey(self) -> None:
-        """Ensure each jersey is assigned to at most one player."""
-        for jersey in JERSEY_POSITIONS:
-            self.problem += 1 >= sum(
-                self.players_in_jerseys[player_name, jersey]
-                for player_name in self.dataset.starting_players
-            )
-
     def solve(self) -> None:
         """Solve the problem."""
         self.problem.solve(PULP_CBC_CMD(msg=0))
+        selected_players = {
+            k: self.players[k].position
+            for k, v in self.players_are_selected.items()
+            if v
+        }
+        positions = POSITIONS.copy()
         self.team = {
-            jersey: player_name
-            for (player_name, jersey), v in self.players_in_jerseys.items()
-            if isinstance(v, LpVariable) and v.value()
+            positions[self.players[name].position].pop(): name
+            for name, position in selected_players.items()
         }
         self.captain = [k for k, v in self.players_are_captain.items() if v.value()][0]
         self.score = self.problem.objective.value()
         self.cost = self.supersub_cost + sum(
-            self.dataset.starting_players[player_name].cost
-            for player_name in self.team.values()
+            self.players[name].cost for name in selected_players
         )
         if self.problem.status != LpStatusOptimal:
             raise ValueError("Optimal solution not found.")
@@ -222,13 +218,10 @@ class Model:
             return
         print("Team optimised.")
         print("\nTeam Sheet:")
-        for jersey in JERSEY_POSITIONS:
-            name = self.team.get(jersey)
+        for number, name in sorted(self.team.items()):
             player_str = f"{name or "---"}{" [C]" if name == self.captain else ""}"
-            country_str = (
-                f" ({self.dataset.starting_players[name].country})" if name else ""
-            )
-            print(f"{jersey:>2}:  {player_str:<24}{country_str}")
+            country_str = f" ({self.players[name].country})" if name else ""
+            print(f"{number:>2}:  {player_str:<24}{country_str}")
         if self.supersub_name:
             print(f"\nSupersub:        {self.supersub_name} ({self.supersub_country})")
         print(f"Expected score:  {self.score:.2f}")
